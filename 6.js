64510fb84f46f7ec8e285136f55d04614700d821
ivallee <isaacvallee@gmail.com>
2018-10-13 10:28:18 -0700

    fix to support 2 digit coordinate

--- 6.js
+++ 6.js
@@ -2,10 +2,9 @@ const { GRID } = require('./grid.js');
 const { convertColumn } = require('./5.js');
 
 function lightCell(coord) {
-  let row = Number(coord[1]) - 1;
+  let row = Number(coord.slice(1)) - 1;
   let col = convertColumn(coord);
   return GRID[row][col];
 }
 
 module.exports = { lightCell };
-
