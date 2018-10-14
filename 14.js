75e5839c6208997b8a14087827b3e3131c78748a
ivallee <isaacvallee@gmail.com>
2018-10-14 10:25:45 -0700

    solve challenge 14

---
+++ 14.js
@@ -0,0 +1,18 @@
+const { GRID } = require('./grid.js');
+const { isShip } = require('./9.js');
+
+function allShips() {
+  const numberToLetter = n => String.fromCharCode(97 + n).toUpperCase();
+  const output = [];
+
+  GRID.forEach((row, i) => {
+    row.forEach((col, j) => {
+      const coord = numberToLetter(j) + (i + 1).toString();
+      if (isShip(coord)) {
+        output.push(coord);
+      }
+    });
+
+  });
+  return output;
+}
